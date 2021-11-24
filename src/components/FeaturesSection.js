b2a1697843be5032cf25b1a26b0a0a12b74e7e71
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 01:49:45 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -53,7 +53,7 @@ export default class FeaturesSection extends React.Component {
                                         'ml-auto': alignX === 'right'
                                     })}
                                     style={{
-                                        backgroundAttachment: "fixed"
+                                        min
                                     }}
                                 />}
                         </div>
